6b6e41ce6f6a44adc11031f5062825001ccab701
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-07-20 14:15:11 +0530

    variable pratices

--- variable/variable.js
+++ variable/variable.js
@@ -1,1 +1,56 @@
 console.log("Hello, World!");
+//console.log("My First js file");
+
+//  fullname = "jyoti prakash";
+//  age = 21 ;
+//  prise = 99.9 ;
+//  x = null ;
+//  console.log(x);
+//  console.log(fullname);
+//  console.log(age);
+//  console.log(prise);
+
+//VARIABLE :
+
+//  let namee = "jyoti prakash behera";
+//  var wife = "Mikuuuuuuuuu";
+//  const number = "26";
+//  const PI = "3.66"
+//  console.log(PI);
+//  console.log(number);
+//  console.log(wife);
+//  console.log(namee);
+
+//DATA TYPE :
+
+//  let age = 21;
+
+// const student = {
+//     Name : "jyoti prakash behera",
+//     Age : 21 ,
+//     cgpa:8.67,
+//     isPass:true,
+// };
+
+// student["Age"]=student["Age"]+1;
+// console.log(student["Age"]);
+
+//Q1.create a const object called "product" to store information show in the picture below.
+
+// const product = {
+//   Name: "pen",
+//   Rating: 4,
+//   offer: 5,
+//   price: 300,
+// };
+// console.log(product);
+
+//Q2.create a const object called "profile" to store information show in the picture below.
+
+const profile ={
+name:"@jyoti",
+isfollo:true,
+following:29999,
+follow:3444,
+};
+console.log(profile);
