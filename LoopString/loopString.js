5ce243f748dd7a940f784c1e3711a885efd015e7
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-07-22 12:58:49 +0530

    update new data

--- LoopString/loopString.js
+++ LoopString/loopString.js
@@ -1,1 +1,50 @@
 console.log("LOOPs");
+
+// //print 1 to 5 loop:
+// for(let i=1;i<=7;i++){
+//     console.log("jyoti");
+// }
+
+//calculate the sum of number0-5?
+
+// let sum = 0;
+// for (let i=1 ; i<=5 ; i++){
+//     sum = sum + i ;
+// }
+// console.log("sum=",sum);//15.
+
+// for(let i = 1 ;i <= 6 ; i++){
+//     console.log("i",i);
+// }
+// print=
+// i=1
+// i=2
+// .
+// .
+// i=6
+
+//While loop:
+
+// let i=1;
+// while(i<=10){
+//     console.log("i",i);
+//     i++;
+// }
+
+// DO WHILE LOOp:
+
+// let i =20;
+// do{
+//     console.log("jyoti");
+//     i++;
+
+// }while(i<=10)
+
+
+
+// FOR-OF-LOOPs:
+
+let str = "jyotiprakash";
+for(let i of str){
+    console.log("i=",i);
+}
