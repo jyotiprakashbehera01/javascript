3974049e74ca7c40b254136833bf27aa69de1634
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-07-28 10:09:41 +0530

    all method

--- LoopString/string.js
+++ LoopString/string.js
@@ -27,5 +27,60 @@
 //console.log("app \n naacollage"); //when use black slace
 // \n - next line .
 // \t -  tab .
-let str = "jyoti\tprakash";//tab has content some space or
-console.log(str.length);
+// let str = "jyoti\tprakash";//tab has content some space or
+// console.log(str.length);
+
+
+//string method example :
+
+
+// let str = "jyoti prakash";
+// str
+// 'jyoti prakash'
+// str.toUperCase
+// undefined
+// str.toUpperCase() It has direct check to console on brouser.
+// 'JYOTI PRAKASH'
+// let str = str.toUpperCase();
+
+// console.log(str);//it can't print.
+// console.log(newstr);//It creat a new valu .
+
+
+// let str = "JYOTI";
+// let newStr = str.toLowerCase();
+
+// console.log(newStr);
+
+// let str="    jyoti prakash    ";
+// console.log(str.trim());
+
+
+
+// let str="0123456";
+// console.log(str.slicei("1","3"));
+
+// let str1="jyoti";
+// let str2="mikii";
+// let res = str1.concat(str2);
+// console.log(res);
+
+
+// let str="hello";
+// // console.log(str.replace("h","p"));
+// console.log(str.replaceAll("hello","jyoti"));
+
+let str="ilovemiki";
+console.log(str.charAt(2));
+
+
+
+
+
+
+
+
+
+
+
+
