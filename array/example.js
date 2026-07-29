70344a2fd79331c853450f4a326ac80824e36c59
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-07-29 23:53:07 +0530

    problem

--- array/example.js
+++ array/example.js
@@ -70,12 +70,76 @@
 // }
 
 
-// for loop approch:
-let iteam = [250, 645, 300, 900, 50];
+// // for loop approch:
+// let iteam = [250, 645, 300, 900, 50];
 
-for (let i = 0; i < iteam.length; i++) {
-    let offer = iteam[i] / 10;
-    iteam[i] -= offer;
-}
+// for (let i = 0; i < iteam.length; i++) {
+//     let offer = iteam[i] / 10;
+//     iteam[i] -= offer;
+// }
 
-console.log(iteam);
+// console.log(iteam);
+
+
+// let mark = [10,20,30,40,50,60,70,80];
+//push opration:
+
+//  mark.push(90);
+// mark.push(100);
+
+//pop operation:
+
+// mark.pop();
+
+//toString:
+
+// mark.toString();
+// console.log(mark);
+
+//conacte operation :
+
+// let hero1 = ["mama" , "bapa" ];
+// let hero2 = ["sis" , "brother"];
+// console.log(hero1.concat(hero2));
+
+// unshift operation :
+
+// let result = [1,2,3,4,5];
+// console.log(result);
+// console.log(result.unshift(6));
+
+//shift operation :
+
+// let arr = [5,6,7,8,9];
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr.length);
+
+
+//slice operation :
+
+// let arr2 = [2,3,4,5,6,7,8,9,2,3,1,0];
+// console.log(arr2.slice(1,6));
+
+//splice operation :
+
+// let cricketer = ["virat","rohit","klrahul","subhman"];
+// console.log(cricketer);
+// console.log(cricketer.splice(1,0,rohit.jyoti));
+
+
+
+/*create an array to store a company name ["bloomerg","microsoft","uber","google","ibm","netflix"]
+a.remove the first in the array?
+b.remove the uber and add the ola?
+c.add amazon at the end ?*/
+
+// ans :
+
+let companyName = ["bloomerg","microsoft","uber","google","ibm","netflix"];//create an array.
+console.log(companyName);
+console.log(companyName.shift());//remove the first company.
+console.log(companyName);
+console.log(companyName.splice(2,1,uber,ola));
+console.log(companyName);
+console.log(companyName.push(amazion));
