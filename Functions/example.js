bb2154c138a6610a46427f572ed2df820ff5d896
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-02 12:34:11 +0530

    check the vowel later

--- Functions/example.js
+++ Functions/example.js
@@ -16,11 +16,43 @@
 
 //Q.create a function of 2 num ?
 
-function sum(a ,b){
-    console.log(a+b);
-    console.log(a*b);
-    console.log(a-b);
-    console.log(a/b);
-    console.log(a%b);
-}
-sum(5,7);
+// function sum(a ,b){
+//     console.log(a+b);
+//     console.log(a*b);
+//     console.log(a-b);
+//     console.log(a/b);
+//     console.log(a%b);
+// }
+// sum(5,7);
+
+//return the function :
+
+// function sumofTwoNo(a,b){
+//     sum = a+b;
+//     return sum;
+// }
+// let val = sumofTwoNo(5,8);
+// console.log(val);//13
+
+//ARROW FUNCTION :
+
+//add
+
+// const arrowSum = ( a , b ) =>{
+//     console.log(a+b);
+// };
+// arrowSum(2,3);
+
+// //multiplication:
+
+// const arrowMultiplication = (a ,b) =>{
+//     console.log(a*b);
+// }
+// arrowMultiplication(5,5);
+
+//print in arrowfunction :
+
+// const arrowHello = () =>{
+//     console.log("hello function");
+// }
+// arrowHello();
---
+++ Functions/question.js
@@ -0,0 +1,36 @@
+//Q1.create a function ,the function key work that a string as an argument 
+//& return the number of vowel of string. ?
+
+//ans-->
+
+//vowel = a,e,i,o,u(5 later).
+//str = "jyptiprakash" // 3 = i,a,a
+
+//creat a function 
+function countVowel(str) {
+    let count = 0;
+
+    // Check every character
+    for (let char of str) {
+        console.log(char);
+
+        // Check whether the character is a vowel
+        if (
+            char === "a" ||
+            char === "e" ||
+            char === "i" ||
+            char === "o" ||
+            char === "u"
+        ) {
+            count++;
+        }
+    }
+
+    // Return total vowel count
+    return count;
+}
+
+// Call the function
+let result = countVowel("jyotiprakash");
+
+console.log("Total vowels:", result);
