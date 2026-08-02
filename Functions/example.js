a0235c7ff931a0ce2682283a7f9183ab84ebc185
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-02 11:45:10 +0530

    basice of function

--- Functions/example.js
+++ Functions/example.js
@@ -1,6 +1,26 @@
-console.log("hello");
+//console.log("hello");
 //string :
-str="abc".toUpperCase();
-console.log(str);
+//str="abc".toUpperCase();
+//console.log(str);
 //array :
-arr=[1,2,3].Push(4);
+//arr=[1,2,3].Push(4);
+
+// Simple example of a function
+// function myfunction() {
+//     console.log("Welcome to my game:");
+//     console.log("You have lost your life:");
+// }
+
+// // Call the function
+// myfunction();
+
+//Q.create a function of 2 num ?
+
+function sum(a ,b){
+    console.log(a+b);
+    console.log(a*b);
+    console.log(a-b);
+    console.log(a/b);
+    console.log(a%b);
+}
+sum(5,7);
